0514e0f79f8b4700b957a3cbde22e093b1a87f09
Võ Văn Thành <vovanthanh2503@gmail.com>
2025-01-11 17:53:24 +0700

    Complete the GUI and project

--- TTCS.py
+++ TTCS.py
@@ -1,56 +1,78 @@
 import tkinter as tk
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, messagebox
 import random
 from sympy import mod_inverse, isprime
 import hashlib
 import time
+class InputData:
+    def __init__(self, root_index):
+        self.root = root_index
+        self.label = tk.Label(root_index, text="Nhập dữ liệu hoặc chọn file: ")
+        self.label.grid(row=0, column=0, padx=5, pady=5)
+        self.entry = tk.Entry(root_index, width=25)
+        self.entry.grid(row=0, column=1, columnspan=2, padx=5, pady=5)
+        self.file_button = tk.Button(root_index, text="Chọn file", command=self.get_file_data)
+        self.file_button.grid(row=0, column=3, padx=5, pady=5)
+        self.file_path = None
+
+    def get_input_data(self):
+        return self.entry.get()
+
+    def get_file_data(self):
+        self.file_path = filedialog.askopenfilename()
+        if self.file_path:
+            try:
+                with open(self.file_path, 'r', encoding='utf-8') as file:
+                    content = file.read()
+                    self.entry.delete(0, tk.END)
+                    self.entry.insert(tk.END, content)
+            except UnicodeDecodeError:
+                try:
+                    with open(self.file_path, 'r', encoding='utf-16') as file:
+                        content = file.read()
+                        self.entry.delete(0, tk.END)
+                        self.entry.insert(tk.END, content)
+                except Exception as e:
+                    messagebox.showerror("Lỗi", f"Không thể mở file: {e}")
+            except Exception as e:
+                messagebox.showerror("Lỗi", f"Không thể mở file: {e}")
+
 class EncryptApp:
     def __init__(self, root_index):
         self.root = root_index
         self.root.title("Ứng dụng mã hóa")
 
-        self.label = tk.Label(root_index, text = "Nhập dữ liệu hoặc chọn file: ")
-        self.label.grid(row = 0, column = 0, padx = 5, pady = 5)
-
-        self.entry = tk.Entry(root_index, width = 25)
-        self.entry.grid(row = 0, column = 1, columnspan = 2, padx = 5, pady = 5)
-
-        self.file_button = tk.Button(root_index, text = "Chọn file", command = self.choose_file)
-        self.file_button.grid(row = 0, column = 3, padx = 5, pady = 5)
+        self.input_data = InputData(root_index)
 
         self.algorithm_label = tk.Label(root_index, text = "Chọn thuật toán mã hóa:")
         self.algorithm_label.grid(row = 1, column = 0, padx = 5, pady = 5)
-
         self.algorithm_var = tk.StringVar(value = "Default")
         self.rsa_radio = tk.Radiobutton(root_index, text = "RSA", variable = self.algorithm_var, value = "RSA")
         self.sha_radio = tk.Radiobutton(root_index, text = "SHA-256", variable = self.algorithm_var, value = "SHA")
         self.rsa_radio.grid(row = 1, column = 1, padx = 5, pady = 5)
         self.sha_radio.grid(row = 1, column = 2, padx = 5, pady = 5)
-
         self.submit_button = tk.Button(root_index, text = "Mã hóa", command = self.submit_data)
         self.submit_button.grid(row = 1, column = 3, padx = 5, pady = 5)
-
         self.file_path = None
 
-    def choose_file(self):
-        self.file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
-        if self.file_path:
-            self.entry.delete(0, tk.END)
-            with open(self.file_path, 'r') as file:
-                self.entry.insert(tk.END, file.read())
-
     def submit_data(self):
-        data = self.entry.get()
+        data = self.input_data.get_input_data()
         algorithm = self.algorithm_var.get()
+
+        if not data:
+            messagebox.showwarning("Lỗi dữ liệu", "Hãy nhập hoặc chọn file chứa dữ liệu cần mã hóa")
+            return
 
         if algorithm == "SHA":
             sha256_root = tk.Tk()
             sha256_app = SHA256App(sha256_root, data)
             sha256_root.mainloop()
-        else:
+        elif algorithm == "RSA":
             rsa_root = tk.Tk()
             rsa_app = RSAEncryptionApp(rsa_root, data)
             rsa_root.mainloop()
+        else:
+            messagebox.showerror("Lỗi thuật toán", "Hãy chọn thuật toán mã hóa dữ liệu")
 
 class BinaryConverter:
     @staticmethod
@@ -88,7 +110,6 @@ class BinaryConverter:
     def xor_string(*strings):
         max_len = max(len(s) for s in strings)
         strings = [s.zfill(max_len) for s in strings]
-
         result = list(strings[0])
         for i in range(1, len(strings)):
             current_str = strings[i]
@@ -100,7 +121,6 @@ class BinaryConverter:
     def and_string(*strings):
         max_len = max(len(s) for s in strings)
         strings = [s.zfill(max_len) for s in strings]
-
         result = list(strings[0])
         for i in range(1, len(strings)):
             current_str = strings[i]
@@ -189,22 +209,15 @@ class SHA256Hash:
 
     def encrypt(self):
         self.prepare_data()
-
         self.data_processing()
-
         self.const = [bin(int(x, 16))[2:].zfill(32) for x in self.const]
-
         self.k_values = [bin(x)[2:].zfill(32) for x in self.k_values]
-
         for i in range(8):
             self.index.append(self.const[i])
-
         for i in range(64):
             self.detail_encrypt(i)
-
         for i in range (8):
             self.const[i] = BinaryConverter.add_binary_string(self.const[i], self.index[i])
-
         encrypt_message = ''.join(HexConverter.binary_to_hex(x) for x in self.const)
         return encrypt_message
 
@@ -233,32 +246,23 @@ class SHA256App(SHA256Hash):
         self.root = root_index
         self.data = string
         self.root.title("Chi tiết hàm băm SHA-256")
-
         button_frame = tk.Frame(self.root)
         button_frame.pack(pady = 10)
-
         self.init_button = tk.Button(button_frame, text = "Dữ liệu ban đầu", command = self.first_data)
         self.init_button.grid(row = 0, column = 0, padx = 5)
-
         self.detail_button = tk.Button(button_frame, text="Chuẩn bị dữ liệu", command=self.detail_data)
         self.detail_button.grid(row=0, column=1, padx=5)
-
         self.process_button = tk.Button(button_frame, text = "Xử lý dữ liệu", command = self.process_data)
         self.process_button.grid(row = 0, column = 2, padx = 5)
-
         self.step_button = tk.Label(button_frame, text = "Kết quả của bước")
         self.step_button.grid(row = 0, column = 3, padx = 5)
-
         self.step_number = ttk.Combobox(button_frame, values = list(range(1, 65)), width = 5)
         self.step_number.grid(row = 0, column = 4, padx = 5)
         self.step_number.bind("<<ComboboxSelected>>", self.get_step_number)
-
         self.final_button = tk.Button(button_frame, text = "Dữ liệu cuối cùng", command = self.final_data)
         self.final_button.grid(row = 0, column = 5, padx = 5)
-
         self.encrypt_button = tk.Button(button_frame, text = "Kết quả mã hóa", command = self.encrypt_data)
         self.encrypt_button.grid(row = 0, column = 6, padx = 5)
-
         self.output_text = tk.Text(root_index, height = 10, width = 100)
         self.output_text.pack(pady = 10)
 
@@ -268,11 +272,9 @@ class SHA256App(SHA256Hash):
         result = result.ljust((length + 64 + 511) // 512 * 512 - 64, '0')
         result += bin(len(self.data.encode('utf-8')) * 8)[2:].zfill(64)
         data_binary = [result[32 * i: 32 * (i + 1)] for i in range(16)]
-
         temp = ""
         for i in range(16):
             temp += f"{data_binary[i]}\n"
-
         self.output_text.delete(1.0, tk.END)
         self.output_text.insert(tk.END, temp)
 
@@ -281,7 +283,6 @@ class SHA256App(SHA256Hash):
         result = ""
         for i in range(64):
             result += f"w[{i}]: {self.w[i]}\n"
-
         self.output_text.delete(1.0, tk.END)
         self.output_text.insert(tk.END, result)
 
@@ -291,7 +292,6 @@ class SHA256App(SHA256Hash):
         result = ""
         for i in range(64):
             result += f"w[{i}]: {self.w[i]}\n"
-
         self.output_text.delete(1.0, tk.END)
         self.output_text.insert(tk.END, result)
 
@@ -304,59 +304,90 @@ class SHA256App(SHA256Hash):
         str2 = f"Không sử dụng thư viện: {hashed_message}\nThời gian thực hiện: {time1} giây\n"
         str3 = f"Sử dụng thư viện: {hashed_message_library}\nThời gian thực hiện: {time2} giây"
         result = str1 + str2 + str3
-
         self.output_text.delete(1.0, tk.END)
         self.output_text.insert(tk.END, result)
 
     def get_step_number(self, event):
         number = int(self.step_number.get())
         result = ""
-
         sha256_hash = SHA256Hash(self.data)
         sha256_hash.encrypt()
-
         index = sha256_hash.all_data[number - 1]
-
         temp = [index[32*i: 32*(i+1)] for i in range(8)]
-
         for i in range(8):
                 result += f"index[{i}]: {temp[i]}\n"
-
         self.output_text.delete(1.0, tk.END)
         self.output_text.insert(tk.END, result)
 
     def final_data(self):
         sha256_variable = SHA256Hash(self.data)
         sha256_variable.prepare_data()
-
         sha256_variable.data_processing()
-
         sha256_variable.const = [bin(int(x, 16))[2:].zfill(32) for x in sha256_variable.const]
-
         sha256_variable.k_values = [bin(x)[2:].zfill(32) for x in sha256_variable.k_values]
-
         for i in range(8):
             sha256_variable.index.append(sha256_variable.const[i])
-
         for i in range(64):
             sha256_variable.detail_encrypt(i)
-
         for i in range(8):
             sha256_variable.const[i] = BinaryConverter.add_binary_string(sha256_variable.const[i], sha256_variable.index[i])
-
         result = ""
         for i in range(8):
             result += f"const[{i}]: {sha256_variable.const[i]} -> Chuyển sang hệ 16: {HexConverter.binary_to_hex(sha256_variable.const[i])}\n"
-
         self.output_text.delete(1.0, tk.END)
         self.output_text.insert(tk.END, result)
 
-class RSAEncryption:
+class NumericTool:
     @staticmethod
     def euclid_algorithm(a, b):
         while b != 0:
             a, b = b, a % b
         return a
+
+    @staticmethod
+    def euclid_gui(a, b):
+        result = [["Lần lặp", "A", "B", "R"]]
+        i = 0
+        while b != 0:
+            r = a % b
+            result.append([f"Lần {i + 1}", a, b, r])
+            a, b = b, r
+            i += 1
+        result.append([f"Lần {i+1}", a, b, 0])
+        return result
+
+    @staticmethod
+    def extended_euclid_gui(a, b):
+        result = [["Lần lặp", "A3", "B3", "R3", "A2", "B2", "R2"]]
+        a1, a2, a3 = 1, 0, a
+        b1, b2, b3 = 0, 1, b
+        i = 0
+        while b3 != 0 and b3 != 1:
+            q = a3 // b3
+            r1 = a1 - q*b1
+            r2 = a2 - q*b2
+            r3 = a3 - q*b3
+            result.append([f"Lần {i + 1}", a3, b3, r3, a2, b2, r2])
+            a1, a2, a3 = b1, b2, b3
+            b1, b2, b3 = r1, r2, r3
+            i += 1
+        return result
+
+    @staticmethod
+    def extended_euclid(a, b):
+        a1, a2, a3 = 1, 0, a
+        b1, b2, b3 = 0, 1, b
+        while b3 != 0 and b3 != 1:
+            q = a3 // b3
+            r1 = a1 - q * b1
+            r2 = a2 - q * b2
+            r3 = a3 - q * b3
+            a1, a2, a3 = b1, b2, b3
+            b1, b2, b3 = r1, r2, r3
+        if b3 == 0:
+            return a3
+        if b3 == 1:
+            return b2
 
     @staticmethod
     def generate_prime_candidate(length = 1024):
@@ -368,22 +399,8 @@ class RSAEncryption:
     def generate_prime_number(length = 1024):
         p = 4
         while not isprime(p):
-            p = RSAEncryption.generate_prime_candidate(length)
+            p = NumericTool.generate_prime_candidate(length)
         return p
-
-    @staticmethod
-    def generate_keypair(length = 1024):
-        p = RSAEncryption.generate_prime_number(length)
-        q = RSAEncryption.generate_prime_number(length)
-        n = p * q
-        phi = (p - 1) * (q - 1)
-        e = random.randrange(1, phi)
-        g = RSAEncryption.euclid_algorithm(e, phi)
-        while g != 1:
-            e = random.randrange(1, phi)
-            g = RSAEncryption.euclid_algorithm(e, phi)
-        d = mod_inverse(e, phi)
-        return (e, n), (d, n), (p, q)
 
     @staticmethod
     def encrypt(pk, plaintext):
@@ -402,18 +419,98 @@ class RSAEncryptionApp:
         self.root = root_index
         self.data = string
         self.root.title("Chi tiết mã hóa RSA")
-
         button_frame = tk.Frame(self.root)
-        button_frame.pack(pady=10)
-
+        button_frame.pack(pady = 2)
         self.length_button = tk.Label(button_frame, text = "Nhập độ dài khóa")
         self.length_button.grid(row = 0, column = 0, padx = 5)
-
         self.length = tk.Entry(button_frame, width = 10)
         self.length.grid(row = 0, column = 1, padx = 5)
+        self.prime_number_button = tk.Button(button_frame, text = "Sinh cặp số nguyên tố", command = self.generate_keypair)
+        self.prime_number_button.grid(row = 0, column = 2, padx = 5)
+        self.gui_button = tk.Button(button_frame, text = "Kiểm tra hai số nguyên tố cùng nhau", command = self.gui_euclid)
+        self.gui_button.grid(row = 0, column = 3, padx = 5)
+        self.inverse_button = tk.Button(button_frame, text = "Tìm phần tử nghịch đảo", command = self.gui_inverse)
+        self.inverse_button.grid(row = 0, column = 4, padx = 5)
+        self.encrypt_button = tk.Button(button_frame, text = "Mã hóa", command = self.encrypt)
+        self.encrypt_button.grid(row = 0, column = 5, padx = 5)
+        self.decrypt_button = tk.Button(button_frame, text = "Giải mã", command = self.decrypt)
+        self.decrypt_button.grid(row = 0, column = 6, padx = 5)
+        self.p = None
+        self.q = None
+        self.N = None
+        self.n = None
+        self.e = None
+        self.g = None
+        self.d = None
+        self.encrypt_message = None
+        self.decrypt_message = None
+        self.output_text = tk.Text(root_index, height = 10, width = 100)
+        self.output_text.pack(pady = 10)
 
-        self.output_text = tk.Text(root_index, height=10, width=100)
-        self.output_text.pack(pady=10)
+    def generate_keypair(self):
+        content = self.length.get()
+        length = 1024
+        if content.strip():
+            length = int(content)
+        self.p = NumericTool.generate_prime_number(length)
+        self.q = NumericTool.generate_prime_number(length)
+        self.N = self.p * self.q
+        self.n = (self.p - 1) * (self.q - 1)
+        result = f"Cặp số nguyên tố: ({self.p}, {self.q})\n -> Chuyển sang hệ 16: ({hex(self.p)[2:]}, {hex(self.q)[2:]})\n"
+        self.output_text.delete(1.0, tk.END)
+        self.output_text.insert(tk.END, result)
+
+    def gui_euclid(self):
+        column_width = [20, 20, 20, 20]
+        self.output_text.delete(1.0, tk.END)
+        self.e = random.randrange(1, self.n)
+        self.g = NumericTool.euclid_algorithm(self.e, self.n)
+        while self.g != 1:
+            self.e = random.randrange(1, self.n)
+            self.g = NumericTool.euclid_algorithm(self.e, self.n)
+        result = NumericTool.euclid_gui(self.e, self.n)
+        headers = result[0]
+        header_row = "".join(f"{str(headers[i]).ljust(column_width[i])}" for i in range(len(headers)))
+        self.output_text.insert("end", header_row + "\n")
+        for row in result[1:]:
+            row_text = "".join(f"{str(row[i]).ljust(column_width[i])}" for i in range(len(row)))
+            self.output_text.insert("end", row_text + "\n")
+        self.output_text.insert("end", f"-> Ước chung lớn nhất là {self.g} nên {self.e} và {self.n} là hai số nguyên tố cùng nhau")
+
+    def gui_inverse(self):
+        column_width = [15, 15, 15, 15, 15, 15, 15]
+        self.output_text.delete(1.0, tk.END)
+        result = NumericTool.extended_euclid_gui(self.n, self.e)
+        temp = NumericTool.extended_euclid(self.n, self.e)
+        if temp >= 0:
+            self.d = temp
+        else:
+            self.d = self.n + temp
+        headers = result[0]
+        header_row = "".join(f"{str(headers[i]).ljust(column_width[i])}" for i in range(len(headers)))
+        self.output_text.insert("end", header_row + "\n")
+        for row in result[1:]:
+            row_text = "".join(f"{str(row[i]).ljust(column_width[i])}" for i in range(len(row)))
+            self.output_text.insert("end", row_text + "\n")
+        self.output_text.insert("end", f"-> Phần tử nghịch đảo của {self.e} trong modulo {self.n} là {self.d}, sử dụng thư viện là {mod_inverse(self.e, self.n)}")
+
+    def encrypt(self):
+        start_time = time.time()
+        self.encrypt_message = [hex(pow(ord(char), self.d, self.N))[2:] for char in self.data]
+        end_time = time.time()
+        time_running = end_time - start_time
+        result = f"Kết quả mã hóa: {''.join(self.encrypt_message)}\nThời gian mã hóa: {time_running} giây"
+        self.output_text.delete(1.0, tk.END)
+        self.output_text.insert(tk.END, result)
+
+    def decrypt(self):
+        start_time = time.time()
+        self.decrypt_message = [chr(pow(int(char, 16), self.e, self.N)) for char in self.encrypt_message]
+        end_time = time.time()
+        time_running = end_time - start_time
+        result = f"Kết quả giải mã: {''.join(self.decrypt_message)}\nThời gian giải mã: {time_running} giây"
+        self.output_text.delete(1.0, tk.END)
+        self.output_text.insert(tk.END, result)
 
 if __name__ == "__main__":
     root = tk.Tk()
